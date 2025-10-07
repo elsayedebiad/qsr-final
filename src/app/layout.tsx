fdd3744f92942503c5083a3d0e9c3bb2549eb0cb
elsayedebiad <153118753+elsayedebiad@users.noreply.github.com>
2025-10-07 04:05:48 +0300

    Fix Google Tag Manager implementation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,43 +23,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ar" dir="rtl" suppressHydrationWarning>
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PQPPR2PP');`,
+          }}
+        />
+      </head>
       <body
         className={`${cairo.variable} font-cairo antialiased`}
         suppressHydrationWarning={true}
       >
-        {/* Google Tag Manager - DataLayer */}
-        <Script
-          id="gtm-datalayer"
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-            `
-          }}
-        />
-        
-        {/* Google Tag Manager */}
-        <Script
-          id="gtm-script"
-          src="https://www.googletagmanager.com/gtm.js?id=GTM-PQPPR2PP"
-          strategy="afterInteractive"
-        />
-        
-        <Script
-          id="gtm-init"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){
-                w[l]=w[l]||[];
-                w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-              })(window,document,'script','dataLayer','GTM-PQPPR2PP');
-            `
-          }}
-        />
-        
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe 
@@ -69,6 +50,7 @@ export default function RootLayout({
             style={{display: 'none', visibility: 'hidden'}}
           />
         </noscript>
+        
         <AuthProvider>
           {children}
         </AuthProvider>
